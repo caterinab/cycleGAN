f8cb46c42db7ccc2b7e43dca627dc3eab625f3c5
unknown <caterina.battisti@studenti.unitn.it>
2019-06-23 10:01:54 +0200

    modify test to output correct attention samples

--- test.py
+++ test.py
@@ -6,12 +6,17 @@ import torchvision
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 import utils
-from arch import define_Gen, define_Dis
+from arch import define_Gen, define_Attn
 
+cudaAvailable = False
+if torch.cuda.is_available():
+    cudaAvailable = True
+Tensor = torch.cuda.FloatTensor if cudaAvailable else torch.Tensor
 
-
-
-
+def toZeroThreshold(x, t=0.1):
+    zeros = Tensor(x.shape).fill_(0.0)
+    return torch.where(x > t, x, zeros)
+	
 def test(args):
 
     transform = transforms.Compose(
@@ -32,13 +37,16 @@ def test(args):
                                                     use_dropout= not args.no_dropout, gpu_ids=args.gpu_ids)
     Gba = define_Gen(input_nc=3, output_nc=3, ngf=args.ngf, netG='resnet_9blocks', norm=args.norm, 
                                                     use_dropout= not args.no_dropout, gpu_ids=args.gpu_ids)
-
+    AttnA = define_Attn()
+    AttnB = define_Attn()
     utils.print_networks([Gab,Gba], ['Gab','Gba'])
 
     #try:
     ckpt = utils.load_checkpoint('%s/latest.ckpt' % (args.checkpoint_dir))
     Gab.load_state_dict(ckpt['Gab'])
     Gba.load_state_dict(ckpt['Gba'])
+    AttnA.load_state_dict(ckpt['AttnA'])
+    AttnB.load_state_dict(ckpt['AttnB'])
     #except:
     #    print(' [*] No checkpoint!')
 
@@ -53,10 +61,31 @@ def test(args):
     Gba.eval()
 
     with torch.no_grad():
-        a_fake_test = Gab(b_real_test)
-        b_fake_test = Gba(a_real_test)
-        a_recon_test = Gab(b_fake_test)
-        b_recon_test = Gba(a_fake_test)
+        # A test
+        attnMapA = toZeroThreshold(AttnA(a_real_test))      
+        fgA = attnMapA * a_real_test                    
+        bgA = (1 - attnMapA) * a_real_test                   
+        genB = Gba(fgA)        
+        b_fake_test = (attnMapA * genB) + bgA
+        b_fake_test_copy = b_fake_test.clone()
+        attnMapfakeB = toZeroThreshold(AttnB(b_fake_test))   
+        fgfakeB = attnMapfakeB * b_fake_test
+        bgfakeB = (1 - attnMapfakeB) * b_fake_test
+        genA_ = Gab(fgfakeB)
+        a_recon_test = (attnMapfakeB * genA_) + bgfakeB         
+
+        # B test
+        attnMapB = toZeroThreshold(AttnB(b_real_test))
+        fgB = attnMapB * b_real_test
+        bgB = (1 - attnMapB) * b_real_test
+        genA = Gab(fgB) 
+        a_fake_test = (attnMapB * genA) + bgB
+        a_fake_test_copy = a_fake_test.clone()
+        attnMapfakeA = toZeroThreshold(AttnA(a_fake_test))
+        fgfakeA = attnMapfakeA * a_fake_test
+        bgfakeA = (1 - attnMapfakeA) * a_fake_test
+        genB_ = Gba(fgfakeA)
+        b_recon_test = (attnMapfakeA * genB_) + bgfakeA
 
     pic = (torch.cat([a_real_test, b_fake_test, a_recon_test, b_real_test, a_fake_test, b_recon_test], dim=0).data + 1) / 2.0
 
@@ -64,4 +93,3 @@ def test(args):
         os.makedirs(args.results_dir)
 
     torchvision.utils.save_image(pic, args.results_dir+'/sample.jpg', nrow=3)
-
